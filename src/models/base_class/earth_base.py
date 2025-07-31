b9339bb215ad59e818c51c8e4beca1e13c58bdff
dangpham12 <phamdangphilong@gmail.com>
2025-07-31 16:49:04 +0200

    earth base class converted

--- src/models/base_class/earth_base.py
+++ src/models/base_class/earth_base.py
@@ -1,10 +1,7 @@
 import numpy as np
-import gt4py.cartesian.gtscript as gtscript
-import gt4py.storage as gt_storage
 
-from src.constants import DTYPE_ACCURACY
-
-Field3D = gtscript.Field[DTYPE_ACCURACY]
+import jax.numpy as jnp
+import jax
 
 class EarthBase:
     """
@@ -12,27 +9,24 @@ class EarthBase:
     The Python implementation of the Earth class to deal with the dunder method, index access, etc...
     There should be no reference to the physical properties of the Earth since it is taken care of in the second layer.
     """
-    water_energy: Field3D
-    water_mass: Field3D
-    air_energy: Field3D
-    air_mass: Field3D
-    land_energy: Field3D
-    land_mass: Field3D
-    chunk_mass: Field3D
-    heat_transfer_coefficient: Field3D
-    specific_heat_capacity: Field3D
-    carbon_ppm: Field3D
-    backend: str
-
+    water_energy: jnp.ndarray
+    water_mass: jnp.ndarray
+    air_energy: jnp.ndarray
+    air_mass: jnp.ndarray
+    land_energy: jnp.ndarray
+    land_mass: jnp.ndarray
+    chunk_mass: jnp.ndarray
+    heat_transfer_coefficient: jnp.ndarray
+    specific_heat_capacity: jnp.ndarray
+    carbon_ppm: jnp.ndarray
 
     def __init__(self, shape: tuple,
-                 water_energy: np.ndarray[DTYPE_ACCURACY] = None,
-                 water_mass: np.ndarray[DTYPE_ACCURACY] = None,
-                 air_energy: np.ndarray[DTYPE_ACCURACY] = None,
-                 air_mass: np.ndarray[DTYPE_ACCURACY] = None,
-                 land_energy: np.ndarray[DTYPE_ACCURACY] = None,
-                 land_mass: np.ndarray[DTYPE_ACCURACY] = None,
-                 backend: str = "numpy",
+                 water_energy: jnp.ndarray = None,
+                 water_mass: jnp.ndarray = None,
+                 air_energy: jnp.ndarray = None,
+                 air_mass: jnp.ndarray = None,
+                 land_energy: jnp.ndarray = None,
+                 land_mass: jnp.ndarray = None,
                  parent=None):
         self.shape = shape
         self.parent = parent
@@ -40,31 +34,29 @@ class EarthBase:
         self._average_temperature = 0
 
         if water_energy is None:
-            water_energy = np.zeros(shape)
+            water_energy = jnp.zeros(shape, dtype=jnp.float32)
         if water_mass is None:
-            water_mass = np.zeros(shape)
+            water_mass = jnp.zeros(shape, dtype=jnp.float32)
         if air_energy is None:
-            air_energy = np.zeros(shape)
+            air_energy = jnp.zeros(shape, dtype=jnp.float32)
         if air_mass is None:
-            air_mass = np.zeros(shape)
+            air_mass = jnp.zeros(shape, dtype=jnp.float32)
         if land_energy is None:
-            land_energy = np.zeros(shape)
+            land_energy = jnp.zeros(shape, dtype=jnp.float32)
         if land_mass is None:
-            land_mass = np.zeros(shape)
+            land_mass = jnp.zeros(shape, dtype=jnp.float32)
 
-        self.water_energy = gt_storage.from_array(water_energy, dtype=DTYPE_ACCURACY, backend=backend)
-        self.water_mass = gt_storage.from_array(water_mass, dtype=DTYPE_ACCURACY, backend=backend)
-        self.air_energy = gt_storage.from_array(air_energy, dtype=DTYPE_ACCURACY, backend=backend)
-        self.air_mass = gt_storage.from_array(air_mass, dtype=DTYPE_ACCURACY, backend=backend)
-        self.land_energy = gt_storage.from_array(land_energy, dtype=DTYPE_ACCURACY, backend=backend)
-        self.land_mass = gt_storage.from_array(land_mass, dtype=DTYPE_ACCURACY, backend=backend)
-        self.chunk_mass = gt_storage.empty(self.shape, dtype=DTYPE_ACCURACY, backend=backend)
-        self.chunk_temp = gt_storage.empty(self.shape, dtype=DTYPE_ACCURACY, backend=backend)
-        self.heat_transfer_coefficient = gt_storage.empty(self.shape, dtype=DTYPE_ACCURACY, backend=backend)
-        self.specific_heat_capacity = gt_storage.empty(self.shape, dtype=DTYPE_ACCURACY, backend=backend)
-        self.carbon_ppm = gt_storage.empty(self.shape, dtype=DTYPE_ACCURACY, backend=backend)
-        self.backend = backend
-        
+        self.water_energy = jnp.array(water_energy)
+        self.water_mass = jnp.array(water_mass)
+        self.air_energy = jnp.array(air_energy)
+        self.air_mass = jnp.array(air_mass)
+        self.land_energy = jnp.array(land_energy)
+        self.land_mass = jnp.array(land_mass)
+        self.chunk_mass = jnp.empty(shape, dtype=jnp.float32) #jnp.empty behaves returns arrays of 0s bc xla compiler cannot create uninitialized arrays
+        self.chunk_temp = jnp.empty(shape, dtype=jnp.float32)
+        self.heat_transfer_coefficient = jnp.empty(shape, dtype=jnp.float32)
+        self.specific_heat_capacity = jnp.empty(shape, dtype=jnp.float32)
+        self.carbon_ppm = jnp.empty(shape, dtype=jnp.float32)
         
 
 
