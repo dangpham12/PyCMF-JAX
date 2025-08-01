82924dd43ded2eb26cdd8afe7270a8a575cdb565
dangpham12 <phamdangphilong@gmail.com>
2025-08-01 17:48:00 +0200

    Ticking Earth finished - now need to debug

--- src/models/ticking_class/ticking_earth.py
+++ src/models/ticking_class/ticking_earth.py
@@ -1,17 +1,10 @@
-import math
-import numpy as np
-from gt4py.cartesian import gtscript
-from gt4py.cartesian.gtscript import PARALLEL, BACKWARD, computation, interval, horizontal, region, I, J, K, IJ, IJK, Field
-import gt4py.storage as gt_storage
+import jax
+import jax.numpy as jnp
+from jax import jit, vmap
 
-from src.constants import DTYPE_ACCURACY
 from src.models.ABC.ticking_model import TickingModel
 from src.models.physical_class.earth import Earth
 
-Field3D = gtscript.Field[DTYPE_ACCURACY]
-backend_opts = dict(
-    device_sync=False  # Only available for GPU backends
-)
 
 class TickingEarth(Earth, TickingModel):
     """
@@ -20,57 +13,63 @@ class TickingEarth(Earth, TickingModel):
 
     /!\ Those methods for update must be marked with @TickingModel.on_tick(enabled=True)
     """
-    def __init__(self, shape: tuple, radius: float = 6.3781e6, *, parent=None, backend="numpy"):
-        Earth.__init__(self, shape, radius, parent=parent, backend=backend)
+    def __init__(self, shape: tuple, radius: float = 6.3781e6, *, parent=None):
+        Earth.__init__(self, shape, radius, parent=parent)
         TickingModel.__init__(self)
         self.time_delta = self.get_universe().TIME_DELTA
         self.evaporation_rate = self.get_universe().EVAPORATION_RATE
 
-        @gtscript.function
-        def temp_coefficient(heat_transfer_coefficient: Field3D,
-                             specific_heat_capacity: Field3D):
-            return heat_transfer_coefficient[0,0,0] * specific_heat_capacity[0,0,0] * self.time_delta
+        @jit # automatically vectorised if inside function being vectorised
+        def coefficient(heat_transfer_coefficient: jnp.float32,
+                             specific_heat_capacity: jnp.float32):
+            return heat_transfer_coefficient * specific_heat_capacity * self.time_delta # constant not modified during runtime
         
-
-        def compute_energy_transfer(in_field: Field3D, energy: Field3D, heat_transfer_coefficient: Field3D, specific_heat_capacity: Field3D):
+        @jit
+        def compute_energy_transfer(chunk_temp: jnp.float32, neighbour_k_up: jnp.float32, neighbour_k_down: jnp.float32,
+                                    neighbour_y_north: jnp.float32, neighbour_y_south: jnp.float32, neighbour_x_east: jnp.float32,
+                                    neighbour_x_west: jnp.float32, coefficient: jnp.float32):
             """
             compute the energy transfer between the grid chunk and its neighbors
             :param grid_chunk:
             :return:
             """
-            with computation(PARALLEL), interval(...):
-                coeff = temp_coefficient(heat_transfer_coefficient, specific_heat_capacity)
-                energy += (in_field[1, 0, 0] - in_field[0, 0, 0]) * coeff
-                energy += (in_field[-1, 0, 0] - in_field[0, 0, 0]) * coeff
-                energy += (in_field[0, 1, 0] - in_field[0, 0, 0]) * coeff
-                energy += (in_field[0, -1, 0] - in_field[0, 0, 0]) * coeff
-                energy += (in_field[0, 0, 1] - in_field[0, 0, 0]) * coeff
-                energy += (in_field[0, 0, -1] - in_field[0, 0, 0]) * coeff
+            energy = 0.0
+            energy += (neighbour_k_up - chunk_temp) * coefficient
+            energy += (neighbour_k_down - chunk_temp) * coefficient
+            energy += (neighbour_y_north - chunk_temp) * coefficient
+            energy += (neighbour_y_south - chunk_temp) * coefficient
+            energy += (neighbour_x_east - chunk_temp) * coefficient
+            energy += (neighbour_x_west - chunk_temp) * coefficient
+            return energy
 
-
-        def water_evaporation(water_mass: Field3D, air_mass: Field3D):
+        @jit
+        def water_evaporation(water_mass: jnp.float32, air_mass: jnp.float32):
             """
             Evaporate water from the water component of the grid chunk
             :param grid_chunk:
             :return:
             """
-            with computation(PARALLEL), interval(...):
-                evaporated_mass = self.evaporation_rate * self.time_delta * water_mass
-                water_mass -= evaporated_mass
-                air_mass += evaporated_mass
+            evaporated_mass = self.evaporation_rate * self.time_delta * water_mass # both constants not modified during runtime
+            water_temp = water_mass - evaporated_mass
+            air_temp = air_mass + evaporated_mass
 
-        def carbon_cycle(carbon_ppm: Field3D, carbon_per_chunk: DTYPE_ACCURACY):
+            return water_temp, air_temp
+
+        @jit
+        def carbon_cycle(carbon_ppm: jnp.float32, carbon_per_chunk: jnp.float32):
             """
             Globally computes carbon flow to be applied to each grid chunk
             :return:
             """
-            with computation(PARALLEL), interval(...):
-                carbon_ppm += carbon_per_chunk
+            carbon_temp = carbon_ppm + carbon_per_chunk
+
+            return carbon_temp
 
 
-        self._water_evaporation = gtscript.stencil(definition=water_evaporation, backend=self.backend, **backend_opts)
-        self._compute_energy_transfer = gtscript.stencil(definition=compute_energy_transfer, backend=self.backend, **backend_opts)
-        self._carbon_cycle = gtscript.stencil(definition=carbon_cycle, backend=self.backend, **backend_opts)
+        self._water_evaporation = vmap(vmap(vmap(water_evaporation)))
+        self._compute_energy_transfer = vmap(vmap(vmap(compute_energy_transfer)))
+        self._carbon_cycle = vmap(vmap(vmap(carbon_cycle)))
+        self._coefficient = vmap(vmap(vmap(coefficient)))
 
     def update(self):
         """
@@ -87,10 +86,18 @@ class TickingEarth(Earth, TickingModel):
         Update the temperature of each grid chunk
         :return:
         """
-        self._compute_chunk_temperature(self.water_energy, self.water_mass, self.air_energy, self.air_mass, self.land_energy, self.land_mass, self.chunk_temp)
-        temp_energy = gt_storage.zeros(self.shape, dtype=DTYPE_ACCURACY, backend=self.backend)
-        self._compute_energy_transfer(self.chunk_temp, temp_energy, self.heat_transfer_coefficient, self.specific_heat_capacity, origin=self.origin)
-        self._add_energy(temp_energy, self.water_energy, self.water_mass, self.air_energy, self.air_mass, self.land_energy, self.land_mass)
+        self.chunk_temp = self._compute_chunk_temperature(self.water_energy, self.water_mass, self.air_energy, self.air_mass, self.land_energy, self.land_mass)
+        temp_coefficient = self._coefficient(self.heat_transfer_coefficient, self.specific_heat_capacity) #unroll the double loop in compute_energy_transfer
+        temp_neighbour_k_up = jnp.roll(self.chunk_temp, shift=-1, axis=0)
+        temp_neighbour_k_down = jnp.roll(self.chunk_temp, shift=1, axis=0)
+        temp_neighbour_y_north = jnp.roll(self.chunk_temp, shift=-1, axis=1)
+        temp_neighbour_y_south = jnp.roll(self.chunk_temp, shift=1, axis=1)
+        temp_neighbour_x_east = jnp.roll(self.chunk_temp, shift=-1, axis=2)
+        temp_neighbour_x_west = jnp.roll(self.chunk_temp, shift=1, axis=2) # shift for every neighbour and parallelized their computation
+        temp_energy = self._compute_energy_transfer(self.chunk_temp, temp_neighbour_k_up, temp_neighbour_k_down,
+                                                    temp_neighbour_y_north, temp_neighbour_y_south, temp_neighbour_x_east,
+                                                    temp_neighbour_x_west, temp_coefficient, origin=self.origin)
+        self.water_energy, self.air_energy, self.land_energy = self._add_energy(temp_energy, self.water_energy, self.water_mass, self.air_energy, self.air_mass, self.land_energy, self.land_mass)
 
 
     @TickingModel.on_tick(enabled=False)
@@ -99,7 +106,7 @@ class TickingEarth(Earth, TickingModel):
         Evaporate water from the water component of the grid chunk
         :return:
         """
-        self._water_evaporation(self.water_mass, self.air_mass)
+        self.water_mass,  self.air_mass = self._water_evaporation(self.water_mass, self.air_mass)
 
         
 
@@ -110,4 +117,4 @@ class TickingEarth(Earth, TickingModel):
         :return:
         """
         carbon_per_chunk = (self.CARBON_EMISSIONS_PER_TIME_DELTA - self.carbon_flux_to_ocean + self.land_carbon_decay - self.biosphere_carbon_absorption) / len(self)
-        self._carbon_cycle(self.carbon_ppm, carbon_per_chunk)
+        self.carbon_ppm = self._carbon_cycle(self.carbon_ppm, carbon_per_chunk)
